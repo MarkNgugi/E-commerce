09cccff3b30fd6562511d9219db274211f6398bc
markmark <mmarkmarkm0@gmail.com>
2024-05-29 17:57:23 +0300

    creating and registering models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50,unique=True)
+
+    def __str__(self):
+        return self.name
+
+#add an image field in the model below:
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    slug=models.SlugField(max_length=200)
+    description=models.TextField(blank=True)
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    available=models.BooleanField(default=True)
+    stock=models.PositiveIntegerField()
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name=models.CharField(max_length=200,null=True)
+    email=models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.id
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product.name
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 def homepage(request):
     homepage = "<H1>CONTENT WILL BE ADDED SOON<H1>"
     return HttpResponse(homepage)
+
+
