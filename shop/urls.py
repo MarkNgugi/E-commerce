0ee2884358d4f743a90d70a48cfe133336ebb13f
markmark <mmarkmarkm0@gmail.com>
2024-05-31 19:34:00 +0300

    adding and modifying pages

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.homecategories,name='homecategories')
+    path('home/',views.homemain,name='homemain'),
+    path('shop/',views.shopmain,name='shopmain')
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 
-def homecategories(request):
+def homemain(request):
     categories = Category.objects.all()
     context = {'categories':categories}
     return render(request,'shop/home.html',context)
+
+def shopmain(request):
+    context={}
+    return render(request,'shop/shopmain.html',context)
 
 
 
