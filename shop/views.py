0a232d39c6a973b8c588247b674e2edbf4493978
markmark <mmarkmarkm0@gmail.com>
2024-05-31 19:47:09 +0300

    small modifications

--- shop/views.py
+++ shop/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse
 from .models import *
 
 def homemain(request):
-    categories = Category.objects.all()
-    context = {'categories':categories}
+    context={}
     return render(request,'shop/home.html',context)
 
 def shopmain(request):
-    context={}
+    categories = Category.objects.all()
+    context = {'categories':categories}
+
     return render(request,'shop/shopmain.html',context)
 
 
