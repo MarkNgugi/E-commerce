cb59b0ae8f65219d827950299f72d092f6701df1
markmark <mmarkmarkm0@gmail.com>
2024-05-31 21:15:45 +0300

    sell product additions

--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('home/',views.homemain,name='homemain'),
     path('shop/',views.shopmain,name='shopmain'),
 
-    path('sellproducts',views.sellproducts,name='sellproducts')
+    path('sellproducts',views.sellproducts,name='sellproducts'),
+    path('success',views.successpage,name='successpage')
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import ProductSellForm
 
 def homemain(request):
     context={}
@@ -13,9 +14,23 @@ def shopmain(request):
     return render(request,'shop/shopmain.html',context)
 
 def sellproducts(request):
-    context={}
-    return render(request,'shop/sellproducts.html',context)
-    
 
+    if request.method=='POST':
+        productsellform=ProductSellForm(request.POST)
+        if productsellform.is_valid():
+            product=productsellform.save()
+            return redirect('successpage')
+        else:
+            pass
+
+    else:
+        productsellform=ProductSellForm()
+
+    context={'productsellform':productsellform}
+    return render(request,'shop/sellproducts.html',context)
+
+def successpage(request):
+    context={}
+    return render(request,'shop/sellsuccess.html',context)
 
 
